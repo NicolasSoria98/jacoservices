f85c1f04a8c9b825eb990ddbc3c46eda7f32771c
Nicolás Soria <105177329+Nebularmess@users.noreply.github.com>
2026-02-05 16:21:16 -0300

    dark

--- src/App.jsx
+++ src/App.jsx
@@ -173,7 +173,7 @@ function App() {
                 </div>
               </a>
             </div>
-
+{/* <div></div> */}
             <div className="cta-footer">
               <svg className="check-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
                 <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" strokeLinecap="round" strokeLinejoin="round"/>
